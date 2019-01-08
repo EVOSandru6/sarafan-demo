054e3b84fe54b98001cccec3529670014967af02
EVOSandru6 <evosduple@mail.ru>
2019-01-09 00:18:09 +0300

    make vue release and google oAuth2

---
+++ src/main/java/com/letscode/sarafan/domain/Message.java
@@ -0,0 +1,54 @@
+package com.letscode.sarafan.domain;
+
+// import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@ToString(of = {"id", "text"})
+@EqualsAndHashCode(of = {"id"})
+// @Data
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Views.Id.class)
+    private long id;
+    @JsonView(Views.IdName.class)
+    private String text;
+
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime created_at;
+
+    public LocalDateTime getCreatedAt() {
+        return created_at;
+    }
+
+    public void setCreatedAt(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/com/letscode/sarafan/repo/MessageRepo.java
@@ -0,0 +1,6 @@
+package com.letscode.sarafan.repo;
+
+import com.letscode.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {}
